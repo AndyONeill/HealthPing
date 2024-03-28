a11b8ad68383ab7d85f7413901bb3a4da2dd3c8a
Andrew <andyo.dev@gmail.com>
2024-03-28 11:04:42 +0000

    Some sketched out views

---
+++ Source/Views/EditPingList/EditPingListView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HealthPing.Views.EditPingList
+{
+    /// <summary>
+    /// Interaction logic for EditPingListView.xaml
+    /// </summary>
+    public partial class EditPingListView : UserControl
+    {
+        public EditPingListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Source/Views/Main/MainWindowViewModel.cs
+++ Source/Views/Main/MainWindowViewModel.cs
@@ -1,12 +1,42 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using HealthPing.Interfaces;
+using HealthPing.Views.EditPingList;
+using HealthPing.Views.ViewPingList;
+using System.Windows;
 
 namespace HealthPing.Views.Main;
 
-public class MainWindowViewModel : ObservableObject
+public partial class MainWindowViewModel : ObservableObject
 {
+    [ObservableProperty]
+    List<ViewVM> views = new List<ViewVM> {
+            new ViewVM{ViewModelType=typeof(ViewPingListViewModel), Name="Ping List" },
+            new ViewVM{ViewModelType=typeof(EditPingListViewModel), Name="Edit List" }
+            };
+
+    [ObservableProperty]
+    Object? currentViewModel;
+
+    [ObservableProperty]
+    ViewVM? selectedView;
+
+    partial void OnSelectedViewChanged(ViewVM? newView)
+    {
+        if (newView?.ViewModelType != null)
+        {
+            CurrentViewModel = Activator.CreateInstance(newView.ViewModelType);
+            if (CurrentViewModel != null && CurrentViewModel is IInitialised)
+            {
+                Application.Current.Dispatcher.InvokeAsync(new Action(async () =>
+                {
+                    await Task.Run(() => ((IInitialised)CurrentViewModel).Initialise());
+                }));
+            }
+        }
+    }
+
+    public MainWindowViewModel() 
+    { 
+        Application.Current.Dispatcher.InvokeAsync(() => { SelectedView = views[0]; });
+    }
 }
