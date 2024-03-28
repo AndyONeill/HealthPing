d638c882f9f0e2cf689bdec1408b519641a70717
Andrew <andyo.dev@gmail.com>
2024-03-28 09:18:35 +0000

    Sketching out project

---
+++ Source/Views/ViewVM.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthPing.Views;
+
+public partial class ViewVM : ObservableObject
+{
+    [ObservableProperty]
+    string name;
+
+    [ObservableProperty]
+    Type viewModelType;
+}
