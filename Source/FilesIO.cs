a2ad370b8ae46ec7e924d675d89b7fc79776b6ea
andyo <andyo.dev@gmail.com>
2024-03-29 11:01:01 +0000

    Added fliesio

---
+++ Source/FilesIO.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace ModelLib;
+
+public static class FilesIO
+{
+    public static string BaseDataDir => Environment.GetFolderPath(
+        Environment.SpecialFolder.LocalApplicationData)
+        + @"\HealthPing\Data";
+
+    public static void SaveType<T>(T obj, string fileName)
+    {
+        string json = JsonSerializer.Serialize(obj);
+        var url = Path.Combine(BaseDataDir, fileName);
+        File.WriteAllText(url, json);
+    }
+
+    public async static Task<T> ReadType<T>(string fileName)
+    {
+        var deserializedObj = Activator.CreateInstance(typeof(T));
+        var url = Path.Combine(BaseDataDir, fileName);
+        if (!File.Exists(url))
+        {
+            return (T)deserializedObj;
+        }
+        using FileStream stream = File.OpenRead(url);
+        return await JsonSerializer.DeserializeAsync<T>(stream);
+    }
+
+}
